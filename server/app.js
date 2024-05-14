792684dc90ea4f6a08bc02fdada16515f1f3677f
Roque Perez <roque_gabriel@hotmail.com>
2024-05-14 14:36:15 +0200

    prueba numero 19381934

--- server/app.js
+++ server/app.js
@@ -11,13 +11,13 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+app.use(cors({ origin: "*" }));
 
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(cors({ origin: "*" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
