c24f2f4ce6b9ec3d0d632be11a70a3c040d4bcee
Roque Perez <roque_gabriel@hotmail.com>
2024-05-15 12:24:02 +0200

    prueba con imagenes subidas

--- client/src/components/Footer/CamigosFooter.jsx
+++ client/src/components/Footer/CamigosFooter.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import "./footer.scss"
+import React from "react";
+import { Button, Container, Nav, Navbar, Row, Col } from "react-bootstrap";
+import "./camigosFooter.scss";
 
 export const CamigosFooter = () => {
   return (
-    <h2>Hola soy el footer.</h2>
-  )
-}
+    <Container id="footer">
+      <Row>
+        <Col className="logos_container">
+          <div className="footer_logo">
+            <img src="/images/logos_rrss/facebook.svg" />
+          </div>
+          <div className="footer_logo">
+            <img src="/images/logos_rrss/instagram.svg" />
+          </div>
+          <div className="footer_logo">
+            <img src="/images/logos_rrss/youTube.svg" />
+          </div>
+          <div className="footer_logo">
+            <img src="/images/logos_rrss/twitter.svg" />
+          </div>
+        </Col>
+        <Col className="disclaimer">
+          <p>
+            Creado por: Gabriel Roque Pérez Torres
+          </p>
+          <p>
+            Familia Canina ©️ UOC-2024
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
--- client/src/components/NavBar/CamigosNavBar.jsx
+++ client/src/components/NavBar/CamigosNavBar.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import React from "react";
+import { Button, Container, Nav, Navbar, Row, Col } from "react-bootstrap";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import "./CamigosNavBar.scss";
+import "./camigosNavBar.scss";
 
 export const CamigosNavBar = () => {
   return (
-<Navbar id="navbar">
-<Container>
-<Nav.Link as={Link} to="/">
-    Home
-</Nav.Link>
-<Nav.Link as={Link} to="/juego">
-    Juego
-</Nav.Link>
-<Nav.Link as={Link} to="/cuidadosParaTuPerro">
-    Cuidados Para Tu Perro
-</Nav.Link>
-<Nav.Link as={Link} to="/finalesFelices">
-    Finales Felices
-</Nav.Link>
-<Nav.Link as={Link} to="/registro">
-    Registro
-</Nav.Link>
-<Nav.Link as={Link} to="/login">
-    Iniciar Sesión
-</Nav.Link>
-</Container>
-</Navbar>  )
-}
+    <Navbar id="navbar">
+      <Container>
+        <Row>
+          <Col className="nav_superior">
+          <Navbar.Brand
+            as={Link}
+            to="/"
+            className="logo_container"
+          >
+            <img src="/camigos_logo.svg" />
+          </Navbar.Brand>
+            <h1 className="titulo_web">
+              Familia&nbsp;Canina
+            </h1>
+          </Col>
+          <Col className="nav_inferior">
+            <Nav.Link as={Link} to="/registro">
+              Registro
+            </Nav.Link>
+            <Nav.Link as={Link} to="/juego">
+              Juego
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cuidadosParaTuPerro">
+              Cuidados Para Tu Perro
+            </Nav.Link>
+            <Nav.Link as={Link} to="/finalesFelices">
+              Finales Felices
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Iniciar Sesión
+            </Nav.Link>
+          </Col>
+        </Row>
+      </Container>
+    </Navbar>
+  );
+};
