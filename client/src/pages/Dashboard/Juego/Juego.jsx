80c81e8fe7797f7412f6c4f75c33be497da07be3
Roque Perez <roque_gabriel@hotmail.com>
2024-05-21 20:18:52 +0200

    puzzle juego

--- client/src/pages/Dashboard/Juego/Juego.jsx
+++ client/src/pages/Dashboard/Juego/Juego.jsx
@@ -1,82 +1,96 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { JigsawPuzzle } from "react-jigsaw-puzzle";
+import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
+import { Button, Container, Col } from "react-bootstrap";
 import axios from "axios";
+import { CamigosContext } from "../../../context/CamigosContext";
+
 import "./juego.scss";
-import { PuzzlePiece } from "./PuzzlePiece";
-
 export const Juego = () => {
-  const [imageSrc, setImageSrc] = useState("");
-  const [pieces, setPieces] = useState([]);
-  const rows = 3;
-  const cols = 3;
+  const { user, isLogged } = useContext(CamigosContext);
+  const [puzzle, setPuzzle] = useState("");
+  const [puzzleRows, setPuzzleRows] = useState(3);
+  const [puzzleColumns, setPuzzleColumns] = useState(3);
+  const [nuevoPuzzle, setNuevoPuzzle] = useState(false);
+  const [verImagen, setVerImagen] = useState(false);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    axios
-      .get("https://dog.ceo/api/breeds/image/random")
-      .then((response) => {
-        setImageSrc(response.data.message);
-        createPuzzlePieces(response.data.message);
-      })
-      .catch((error) => console.log(error));
-  }, []);
+  const handleDificultad = (dificultad) => {
+    puzzle == "" &&
+      axios
+        .get("https://dog.ceo/api/breeds/image/random")
+        .then((response) => {
+          setPuzzle(response.data.message);
+        })
+        .catch((error) => console.log(error));
 
-  const createPuzzlePieces = (imageSrc) => {
-    const piecesArray = [];
-    const pieceWidth = 100 / cols; 
-    const pieceHeight = 100 / rows;
-
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
-        piecesArray.push({
-          id: `${row}-${col}`,
-          row,
-          col,
-          width: pieceWidth,
-          height: pieceHeight,
-          backgroundImage: imageSrc,
-          backgroundPosition: `${-col * pieceWidth}% ${-row * pieceHeight}%`,
-          order: row * cols + col,
-        });
-      }
-    }
-
-    piecesArray.sort(() => Math.random() - 0.5);
-    setPieces(piecesArray);
+    setNuevoPuzzle(true);
+    setPuzzleRows(dificultad);
+    setPuzzleColumns(dificultad);
   };
 
-  const handlePieceDrop = (id, newRow, newCol) => {
-    const newPieces = pieces.map((piece) => {
-      if (piece.id === id) {
-        return { ...piece, row: newRow, col: newCol };
-      }
-      return piece;
-    });
-    setPieces(newPieces);
-    if (checkWinCondition(newPieces)) {
-      alert("¡Has ganado!");
-    }
-  };
-
-  const checkWinCondition = (pieces) => {
-    return pieces.every(
-      (piece, index) => piece.id === `${Math.floor(index / cols)}-${index % cols}`
-    );
-  };
+  useEffect(() => {}, []);
 
   return (
-    <div className="puzzle-game">
-      <div className="puzzle-board">
-        {pieces.map((piece, index) => (
-          <PuzzlePiece
-            key={piece.id}
-            piece={piece}
-            onDrop={handlePieceDrop}
-            index={index}
-            cols={cols}
-            rows={rows}
-          />
-        ))}
-      </div>
-    </div>
+    <Container className="contenedor_puzzle">
+      {!isLogged && (
+        <Col>
+          <p>
+            Si te creas un usuario, ¡podrás guardar los puzzles que resuelvas!
+            ve ya a{" "}
+            <a
+              className="registro_enlace"
+              onClick={() => navigate("/registro")}
+            >
+              Registro
+            </a>
+          </p>
+        </Col>
+      )}
+      <Col className="cabecera_puzzle">
+        <div className="instrucciones">
+          <p>
+            Arrastra las piezas del puzzle hasta que se encaje en la parte
+            correcta del marco. Sabrás que está en la posición correcta porque
+            ya no puedes arrastrar esa pieza. Lo más sencillo, como suele pasar,
+            es empezar por las esquinas. ¡Diviértete!
+          </p>
+        </div>
+        <div className="nuevo_puzzle">
+          <h3>Nuevo Puzzle</h3>
+          <div className="opciones">
+            <Button onClick={() => handleDificultad(3)}>Fácil</Button>
+            <Button className="medio" onClick={() => handleDificultad(6)}>
+              Medio
+            </Button>
+            <Button className="dificil" onClick={() => handleDificultad(9)}>
+              Difícil
+            </Button>
+            <Button className="legendario" onClick={() => handleDificultad(15)}>
+              Legendario
+            </Button>
+          </div>
+        </div>
+      </Col>
+      {nuevoPuzzle && (
+        <Col className="imagen_referencia">
+          <button onClick={() => setVerImagen(!verImagen)}>
+            Ver imagen de referencia
+          </button>
+          {verImagen && <img className="superpuesta" src={puzzle}></img>}
+          {verImagen && <img className="cuadradito" src={puzzle}></img>}
+        </Col>
+      )}
+      {nuevoPuzzle && (
+        <JigsawPuzzle
+          className="puzzle_perros"
+          imageSrc={`${puzzle}`}
+          rows={puzzleRows}
+          columns={puzzleColumns}
+          onSolved={() => alert("Solved!")}
+        />
+      )}
+    </Container>
   );
 };
-
